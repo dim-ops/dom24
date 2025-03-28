3757dff5f9b7d37189612fae3041f091902257e1
Dimitri GRISARD <dimitri.grisard@ingenico.com>
2025-03-28 20:07:09 +0100

    fix: public subnets cicd

--- terraform/prod/10_vpc/main.tf
+++ terraform/prod/10_vpc/main.tf
@@ -11,7 +11,7 @@ module "vpc" {
 
   azs             = ["eu-south-2a", "eu-south-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.101.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   # Internet Gateway
   enable_nat_gateway = true # vpc to internet
